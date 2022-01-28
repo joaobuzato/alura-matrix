cfeae16a69693d6a2819effc55eb9fb46f0d0b8f
Buzato <joaobuzato@gmail.com>
2022-01-28 10:39:50 -0300

    aula 4 react

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-
+import appConfig from '../config.json'
 function GlobalStyle() {
     return (
         <style global jsx>{`
@@ -22,6 +22,19 @@ function GlobalStyle() {
         }
         #__next > * {
           flex: 1;
+        }
+        ul::-webkit-scrollbar {
+          width: 12px;               /* width of the entire scrollbar */
+        }
+        
+        ul::-webkit-scrollbar-track {
+          display: none
+        }
+        
+        ul::-webkit-scrollbar-thumb {
+          background-color:  ${appConfig.theme.colors.neutrals['900']};    /* color of the scroll thumb */
+          border-radius: 20px;       /* roundness of the scroll thumb */
+          border: 3px solid  ${appConfig.theme.colors.neutrals['700']};  /* creates padding around scroll thumb */
         }
         /* ./App fit Height */ 
       `}</style>
--- pages/chat.js
+++ pages/chat.js
@@ -48,7 +48,7 @@ export default function ChatPage() {
             styleSheet={{
                 display: 'flex', alignItems: 'center', justifyContent: 'center',
                 backgroundColor: appConfig.theme.colors.primary[500],
-                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/earthrise.jpg)`,
                 backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 color: appConfig.theme.colors.neutrals['000']
             }}
@@ -145,7 +145,7 @@ function MessageList(props) {
         <Box
             tag="ul"
             styleSheet={{
-                overflow: 'scroll',
+                overflow: 'auto',
                 display: 'flex',
                 flexDirection: 'column-reverse',
                 flex: 1,
